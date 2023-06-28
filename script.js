528e43d39e2ad30daed5357d7e1e92ce673900ed
Akangkha <akangkhasarkar@gmail.com>
2023-06-28 15:31:29 +0530

    animations added

--- script.js
+++ script.js
@@ -3,24 +3,35 @@ window.onmousedown = (e) => {
   track.dataset.mouseDownAt = e.clientX;
 };
 
+const img = track.getElementsByClassName("image");
+console.log(img);
+
 window.onmousemove = (e) => {
   if (track.dataset.mouseDownAt == "0") return;
 
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
 
-  const percentage = (mouseDelta / maxDelta) * 100;
+  const percentage = (mouseDelta / maxDelta) * -100;
   const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
   track.dataset.percentage = nextPercentage;
-  track.style.transform = `translate(${percentage}%,  0%)`;
+  // track.style.transform = `translate(${percentage}%,  0%)`;
+  track.animate({
+    transform:`translate(${nextPercentage}%,  0%)`
+  },
+  {duration:1200,fill:"forwards"});
   for (const image of track.getElementsByClassName("image")) {
-    console.log(nextPercentage);
-
-    image.style.objectPosition = `${nextPercentage + 100} 50%`;
+    // image.style.objectPosition = `${nextPercentage + 100}% 50%`;
+    //  image.style.filter = `brightness(50%)`;
+    // console.log(nextPercentage + 100);
+    image.animate({
+      objectPosition : `${nextPercentage + 100}% 50%`
+    },
+    {duration:1200,fill:"forwards"});
   }
 };
 
 window.onmouseup = () => {
   track.dataset.mouseDownAt = "0";
-  trackEvent.dataset.prevPercentage = track.dataset.percentage;
+  track.dataset.prevPercentage = track.dataset.percentage;
 };
